58017be7de4465ad8a69ab730c543b7f32ae7b9d
Alon Galperin <alon@Alons-MacBook-Air.local>
2024-05-06 20:18:27 +0300

    change title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>asfasasf</p>
+        <p>second commit</p>
         <a
           className="App-link"
           href="https://reactjs.org"
